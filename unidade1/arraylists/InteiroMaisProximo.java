5b3ed6cbf08bd84445a4e088e503eb8230157033
Glaydson <glaydsonvs@gmail.com>
2020-08-18 16:15:52 -0300

    Inteiro mais próximo

---
+++ unidade1/arraylists/InteiroMaisProximo.java
@@ -0,0 +1,22 @@
+package unidade1.arraylists;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class InteiroMaisProximo {
+    public static void main(String[] args) {
+        // Escreva seu código aqui
+       
+    }
+
+    // Não modifique este método
+    private static ArrayList<Integer> readArrayList(Scanner scanner) {
+        return Arrays
+                .stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+}
