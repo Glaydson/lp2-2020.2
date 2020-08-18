158353c1b9094681bffadebf616486aecbece1fe
Glaydson <glaydsonvs@gmail.com>
2020-08-18 16:03:32 -0300

    Primeiro commit

---
+++ unidade1/arraylists/Concat.java
@@ -0,0 +1,40 @@
+package unidade1.arraylists;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Concat {
+
+    public static ArrayList<Integer> concatNumerosPositivos(ArrayList<Integer> l1, ArrayList<Integer> l2) {
+        ArrayList<Integer> result = new ArrayList<>();
+        for (Integer x : l1) {
+            if (x > 0)
+                result.add(x);
+        }
+        for (Integer x : l2) {
+            if (x > 0)
+                result.add(x);
+        }
+        return result;
+    }
+
+    /* Não modifique este método */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Integer> list1 = readArrayList(scanner);
+        ArrayList<Integer> list2 = readArrayList(scanner);
+
+        ArrayList<Integer> result = concatNumerosPositivos(list1, list2);
+
+        result.forEach(n -> System.out.print(n + " "));
+    }
+
+    /* Não modifique este método */
+    private static ArrayList<Integer> readArrayList(Scanner scanner) {
+        return Arrays.stream(scanner.nextLine().split("\\s+")).map(Integer::parseInt)
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+}
