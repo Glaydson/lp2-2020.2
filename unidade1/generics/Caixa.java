e4c12cd5dbde074ce0c77503511d3130a764cbdd
Glaydson <glaydsonvs@gmail.com>
2020-08-18 22:36:20 -0300

    Inclusão de Caixa.java

---
+++ unidade1/generics/Caixa.java
@@ -0,0 +1,20 @@
+package unidade1.generics;
+
+
+/**
+
+Classe Caixa, baseada em Object, requerendo o cast toda vez que se desejar obter algum conteúdo específico, como por exemplo:
+
+    Caixa caixaBolo = new Caixa();
+    caixaBolo.put(new Bolo());
+    // Sem o cast, somente o objeto Bolo pode ser retirado da caixa
+    Bolo bolo = (Bolo) caixaBolo.get();
+
+Casting não é desejável e pode falhar em tempo de execução. Construa uma solução mais genérica…
+
+*/
+
+
+public class Caixa {
+    
+}
