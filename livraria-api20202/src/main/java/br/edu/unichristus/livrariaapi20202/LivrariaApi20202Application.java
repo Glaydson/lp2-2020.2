2aa878102add853eb3f7848549fd54bb9f7b606f
glaydson <glaydsonvs@gmail.comm>
2020-09-15 21:57:04 -0300

    Criação da API livraria

---
+++ livraria-api20202/src/main/java/br/edu/unichristus/livrariaapi20202/LivrariaApi20202Application.java
@@ -0,0 +1,13 @@
+package br.edu.unichristus.livrariaapi20202;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivrariaApi20202Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivrariaApi20202Application.class, args);
+	}
+
+}
